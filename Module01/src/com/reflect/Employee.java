066d1c1548d2be9660b4c78c442428ce85c508f3
Iam <1974410293@qq.com>
2019-09-07 15:00:08 +0800

    # JavaDemo

--- Module01/src/com/reflect/Employee.java
+++ Module01/src/com/reflect/Employee.java
@@ -5,8 +5,20 @@ package com.reflect;
  * @create 2019-09-07 8:40
  */
 public class Employee {
-    public static void main(String[] args)
-    {
-        System.out.println("hhh");
-    }
+        //Field
+        private String name;
+
+        //Constructor
+        public Employee(){}
+
+        public Employee(String name)
+        {
+            this.name = name;
+        }
+
+        //方法
+        public void m1(){
+            System.out.println(name+"哈喽 世界");
+        }
+
 }
--- Module01/src/com/reflect/ReflectTest01.java
+++ Module01/src/com/reflect/ReflectTest01.java
@@ -4,8 +4,14 @@ package com.reflect;
  * @author shkstart
  * @create 2019-09-07 8:38
  */
+/*
+    获取Class类型对象的三种方式
+ */
 public class ReflectTest01 {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args) throws Exception{
+        //第一种方式、
+        //c1引用保存的内存地址指向堆中的对象，该对象代表的是Employee整个类。
+        Class c1 = Class.forName("Employee");
+
     }
 }
